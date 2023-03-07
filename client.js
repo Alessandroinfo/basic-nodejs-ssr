80e4283e5dfc28de5001213ed9bdeab74741dfb0
Alessandro Russo <alessandro.russo@datlas.it>
2023-03-07 12:29:11 +0100

    SSR in Node.js

--- client.js
+++ client.js
@@ -2,8 +2,10 @@ console.log('[Client log] This script will run on the client.');
 
 const hi = document.createElement('div');
 hi.innerHTML = `
+<div class="card client">
 <h1>Hi, i was manipulated on client side!</h1>
 <h2>To see edits you only need to modify the js files</h2>
+</div>
 `;
 const app = document.querySelector('#app');
 app.appendChild(hi);
--- server.js
+++ server.js
@@ -12,10 +12,11 @@ const server = http.createServer((req, res) => {
                     res.end(`Error getting the file: ${err}.`);
                 } else {
                     const content = `
+                    <div class="card server">
                     <h1>Hello, World!</h1>
                     <h2>The whole page was rendered on server side!</h2>
                     <h2>You need to re-run server to see the edits</h2>
-                    `;
+                    </div>`;
                     const renderedHtml = html.replace('<!-- This is where the server will render the content -->', content);
                     res.statusCode = 200;
                     res.setHeader('Content-Type', 'text/html');
